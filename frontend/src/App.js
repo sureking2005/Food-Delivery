d991992ccc16f3a30179e13118901d215ea66aef
idhaya13 <mariaidhayainfancia.x@gmail.com>
2025-01-25 13:09:51 +0530

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Role  from './pages/Role';
 import UserGuest from './pages/UserGuest';
 import DeliveryboyLogin from './pages/Deliveryboy/DeliveryboyLogin';
-
+import UserHome from './pages/User/UserHome';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Route path="/" element={<Role/>}/>
         <Route path="/userguest" element={<UserGuest/>}/>
         <Route path="/deliveryboylogin" element={<DeliveryboyLogin/>}/>
+        <Route path="/userhome" element={<UserHome/>}/>
 
 
       </Routes>
